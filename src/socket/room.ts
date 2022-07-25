7882697afe7d8afe5b21b5e63b4e2e153683c05d
Alexander Khapchenko <alexanderfox1337@gmail.com>
2022-07-25 17:19:25 +0300

    Added event constants

--- src/socket/room.ts
+++ src/socket/room.ts
@@ -6,23 +6,24 @@ import {texts} from '../data';
 import {Room} from '../services/room';
 import {TCommentator} from "../component/commentator/commentator";
 import {IResult} from "../interfaces/result";
+import {Events} from "../constants/enums/events";
 
 export default (io: Server, socket: Socket, commentator: TCommentator) => {
 	const room = new Room(io);
 
 	const rooms = room.getAllRoom();
 
-	socket.emit("GET_EXISTS_ROOM", {rooms: rooms});
+	socket.emit(Events.GET_EXISTS_ROOM, {rooms: rooms});
 
-	socket.on("CREATE_ROOM", ({roomName, username}) => {
+	socket.on(Events.CREATE_ROOM, ({roomName, username}) => {
 		if(room.isRoomExist(roomName)) {
-			socket.emit('ROOM_EXISTS', `Room name ${roomName} already used`);
+			socket.emit(Events.ROOM_EXISTS, `Room name ${roomName} already used`);
 		}
 		else {
 			socket.join(roomName);
-			socket.emit("UPDATE_ROOMS", {numberOfUsers: 1, roomName});
+			socket.emit(Events.UPDATE_ROOMS, {numberOfUsers: 1, roomName});
 			const numberOfUsers = room.getNumberOfUsers(roomName);
-			socket.broadcast.emit("UPDATE_ROOMS", {numberOfUsers, roomName});
+			socket.broadcast.emit(Events.UPDATE_ROOMS, {numberOfUsers, roomName});
 			tryJoinRoom({roomName, username});
 		}
 	});
@@ -37,25 +38,25 @@ export default (io: Server, socket: Socket, commentator: TCommentator) => {
 		const isFullRoom = room.isRoomFull(roomName);
 
 		if (!room.isRoomInGame(roomName)) {
-			socket.broadcast.emit("UPDATE_ROOM_LIST", {roomName, numberOfUsers, isFullRoom});
-			socket.emit("UPDATE_ROOM_LIST", {roomName, numberOfUsers, isFullRoom});
+			socket.broadcast.emit(Events.UPDATE_ROOM_LIST, {roomName, numberOfUsers, isFullRoom});
+			socket.emit(Events.UPDATE_ROOM_LIST, {roomName, numberOfUsers, isFullRoom});
 			messageByCommentator(commentator.getHello(username), roomName);
 		}
 
-		socket.emit("JOIN_ROOM_DONE", {roomName, existUsers, newUser});
-		socket.in(roomName).emit("JOIN_ROOM_DONE", {roomName, newUser});
+		socket.emit(Events.JOIN_ROOM_DONE, {roomName, existUsers, newUser});
+		socket.in(roomName).emit(Events.JOIN_ROOM_DONE, {roomName, newUser});
 	}
 
-	socket.on("TRY_JOIN_ROOM", tryJoinRoom);
+	socket.on(Events.TRY_JOIN_ROOM, tryJoinRoom);
 
-	socket.on("UPDATE_USER", (updateFields: Partial<IUser>) => {
+	socket.on(Events.UPDATE_USER, (updateFields: Partial<IUser>) => {
 		const user = Users.getOne({id: socket.id});
 		if(user) {
 			const {name, activeRoom} = user;
 			const newUser = Users.update({name, updateFields});
 			if (activeRoom) {
-			 socket.to(activeRoom).emit("UPDATE_USER_IN_ROOM", {user: newUser});
-			 socket.emit("UPDATE_USER_IN_ROOM", {user: newUser});
+			 socket.to(activeRoom).emit(Events.UPDATE_USER_IN_ROOM, {user: newUser});
+			 socket.emit(Events.UPDATE_USER_IN_ROOM, {user: newUser});
 			}
 
 			if('progress' in updateFields && activeRoom) {
@@ -100,15 +101,15 @@ export default (io: Server, socket: Socket, commentator: TCommentator) => {
 
 	const readyToGame = ({activeRoom}) => {
 		room.gameInProgress(activeRoom);
-		socket.broadcast.emit("DELETE_ROOM", {roomName: activeRoom});
+		socket.broadcast.emit(Events.DELETE_ROOM, {roomName: activeRoom});
 
 		const usersInCurrentRoom = Users.getAll().filter(user => user.activeRoom == activeRoom);
 		messageByCommentator(commentator.getHelloForAll(usersInCurrentRoom), activeRoom);
 
 		let sec = config.SECONDS_TIMER_BEFORE_START_GAME;
 		const interval = () => {
-			socket.to(activeRoom).emit("READY_TO_GAME", {sec});
-			socket.emit("READY_TO_GAME", {sec});
+			socket.to(activeRoom).emit(Events.READY_TO_GAME, {sec});
+			socket.emit(Events.READY_TO_GAME, {sec});
 			sec--;
 		}
 		interval();
@@ -129,14 +130,14 @@ export default (io: Server, socket: Socket, commentator: TCommentator) => {
 		let secForCommentator = 0;
 		const randomText = texts[Math.floor(Math.random() * texts.length)];
 
-		socket.to(activeRoom).emit("START_GAME", {text: randomText});
-		socket.emit("START_GAME", {text: randomText});
+		socket.to(activeRoom).emit(Events.START_GAME, {text: randomText});
+		socket.emit(Events.START_GAME, {text: randomText});
 
 		const usersInCurrentRoom = Users.getAll().filter(user => user.activeRoom == activeRoom);
 
 		const interval = () => {
-			socket.to(activeRoom).emit("UPDATE_GAME_TIMER", {sec});
-			socket.emit("UPDATE_GAME_TIMER", {sec});
+			socket.to(activeRoom).emit(Events.UPDATE_GAME_TIMER, {sec});
+			socket.emit(Events.UPDATE_GAME_TIMER, {sec});
 			room.setSecondInGame(activeRoom, secForCommentator);
 
 			if(secForCommentator % config.COMMENTATOR_SAY_ANYTHING_EVERY_SECONDS == 0) {
@@ -199,8 +200,8 @@ export default (io: Server, socket: Socket, commentator: TCommentator) => {
 			});
 
 			messageByCommentator(commentator.getResultForAll(results), activeRoom);
-			socket.to(activeRoom).emit("GAME_OVER");
-			socket.emit("GAME_OVER");
+			socket.to(activeRoom).emit(Events.GAME_OVER);
+			socket.emit(Events.GAME_OVER);
 		}
 	}
 
@@ -216,7 +217,7 @@ export default (io: Server, socket: Socket, commentator: TCommentator) => {
 				time: undefined
 			}});
 
-			socket.to(roomName).emit("REMOVE_USER_ELEMENT", {username});
+			socket.to(roomName).emit(Events.REMOVE_USER_ELEMENT, {username});
 			socket.leave(roomName);
 
 			const numberOfUsers = room.getNumberOfUsers(roomName);
@@ -230,20 +231,20 @@ export default (io: Server, socket: Socket, commentator: TCommentator) => {
 				checkGameOver({activeRoom: roomName});
 			}
 
-			socket.broadcast.emit("UPDATE_ROOM_LIST", {roomName, numberOfUsers: numberOfUsers, isFullRoom});
-			socket.emit("UPDATE_ROOM_LIST", {roomName, numberOfUsers: numberOfUsers, isFullRoom});
+			socket.broadcast.emit(Events.UPDATE_ROOM_LIST, {roomName, numberOfUsers: numberOfUsers, isFullRoom});
+			socket.emit(Events.UPDATE_ROOM_LIST, {roomName, numberOfUsers: numberOfUsers, isFullRoom});
 
 			if(!room.isRoomExist(roomName)) {
-				socket.emit("DELETE_ROOM", {roomName});
-				socket.broadcast.emit("DELETE_ROOM", {roomName});
+				socket.emit(Events.DELETE_ROOM, {roomName});
+				socket.broadcast.emit(Events.DELETE_ROOM, {roomName});
 				room.roomDelete(roomName);
 			}
 		}
 	}
 
-	socket.on("EXIT_ROOM", exitRoom);
+	socket.on(Events.EXIT_ROOM, exitRoom);
 
-	socket.on("disconnect", () => {
+	socket.on(Events.DISCONNECT, () => {
 		const user = Users.getOne({id: socket.id});
 
 		if(user && 'activeRoom' in user) {
@@ -251,7 +252,7 @@ export default (io: Server, socket: Socket, commentator: TCommentator) => {
 		}
 	});
 
-	socket.on("COMMENTATOR_SYMBOL_INFO", () => {
+	socket.on(Events.COMMENTATOR_SYMBOL_INFO, () => {
 		const user = Users.getOne({id: socket.id});
 		if (user) {
 			messageByCommentator(commentator.getSymbolInfo(user.name), user.activeRoom);
@@ -259,6 +260,6 @@ export default (io: Server, socket: Socket, commentator: TCommentator) => {
 	});
 
 	const messageByCommentator = (message, roomId) => {
-		io.to(roomId).emit("COMMENTATOR_SAY", message);
+		io.to(roomId).emit(Events.COMMENTATOR_SAY, message);
 	}
 }
--- src/socket/user.ts
+++ src/socket/user.ts
@@ -1,19 +1,20 @@
 import { Users } from "../services/users";
 import { Socket } from 'socket.io';
 import {CarProxy} from "../component/commentator/utils/car-proxy";
+import {Events} from "../constants/enums/events";
 
 export default (socket: Socket) => {
 	const username = socket.handshake.query.username;
 	const carProxy = new CarProxy();
 
 	if (Users.getOne({name: username as string})) {
-		socket.emit('USER_EXIST', `Username ${username} already used`);
+		socket.emit(Events.USER_EXIST, `Username ${username} already used`);
 	}
 	else {
 		Users.create({name: username as string, id: socket.id, ready: false, progress: 0, totalRace: 0, car: carProxy});
 	}
 
-	socket.on("disconnect", () => {
+	socket.on(Events.DISCONNECT, () => {
 		const user = Users.getOne({id: socket.id});
 		if(user && user.name) {
 			Users.remove(user!.name);
