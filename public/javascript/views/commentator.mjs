270ea39b7a2100ed58a620a47e947885a0c1cea5
Alexander Khapchenko <alexanderfox1337@gmail.com>
2022-07-25 12:10:52 +0300

    Changed text to html

--- public/javascript/views/commentator.mjs
+++ public/javascript/views/commentator.mjs
@@ -1,4 +1,4 @@
 export const messageByCommentator = (text) => {
     const commentatorText = document.getElementById('comment');
-    commentatorText.innerText = text;
+    commentatorText.innerHTML = text;
 }
